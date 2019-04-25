98bcf9c7d0f3b13c57d9f8ac2b3084fd6fbcbb45
Allakazan <brunorizzardi80014@hotmail.com>
2019-04-24 23:54:39 -0300

    Mocha + Chai + Karma test environment

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
-const arr = [1, 2, 3];
-const iAmJavascriptES6 = () => console.log(...arr);
-window.iAmJavascriptES6 = iAmJavascriptES6;
+/*
+ * Lightweight JS Image Editor written in pure js
+
+Copyright (c) 2019 Bruno Marques
+
+ */
+(function () {
+    'use strict';
+
+    function MagikJs() { }
+    MagikJs.prototype = {
+
+    }
+
+    // amd
+    if (typeof define !== 'undefined' && define.amd) define(function () { return MagikJs; });
+    // common js
+    if (typeof exports !== 'undefined') exports.MagikJs = MagikJs;
+    // browser
+    else if (typeof window !== 'undefined') window.MagikJs = MagikJs;
+    // nodejs
+    if (typeof module !== 'undefined') {
+        module.exports = MagikJs;
+    }
+    
+})();
---
+++ test/app.spec.js
@@ -0,0 +1,7 @@
+describe('app', function () {
+    describe('#MagickJs()', function () {
+        it('should return the library object', function () {
+            expect(MagikJs).to.be.a('function');
+        });
+    });
+});
